528f1e5e92c17b5e29541ddd3ec7e343f3a8b595
prasadhanwadikar <prasad.hanwadikar@gmail.com>
2019-03-02 20:12:16 -0800

    Db schema update

--- Db/Tables/QuoteAssets.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[QuoteAssets] (
-    [Id]     INT           IDENTITY (1, 1) NOT NULL,
-    [UserId] INT           NOT NULL,
-    [Asset]  VARCHAR (10) NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_QuoteAssets_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id])
-);
-
